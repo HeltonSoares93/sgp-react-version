70980c528336abf5c3ad53314978517d5c9180ab
Helton Soares <hgsdl@live.com>
2026-03-22 17:46:31 -0300

    Conclusão da conexão do dashboard com o backend

--- src/componentes/Dashboard.jsx
+++ src/componentes/Dashboard.jsx
@@ -36,10 +36,24 @@ export default function Dashboard() {
     carregarDados();
   }, [])
 
+  
   const totalAtivo = projetos?.filter(p => p.status === 'ATIVO').length || 0;
   const totalSuspenso = projetos?.filter(p => p.status === 'SUSPENSO').length || 0;
   const totalInativo = projetos?.filter(p => p.status === 'INATIVO').length || 0;
   const totalCancelado = projetos?.filter(p => p.status === 'CANCELADO').length || 0;
+  
+  const totalTarefaAtivo = tarefas?.filter(p => p.status === 'EM_ANDAMENTO').length || 0;
+  const totalTarefaConcluido = tarefas?.filter(p => p.status === 'CONCLUIDO').length || 0;
+  const totalTarefaSuspenso = tarefas?.filter(p => p.status === 'SUSPENSO').length || 0;
+  const totalTarefaCancelado = tarefas?.filter(p => p.status === 'CANCELADO').length || 0;
+  const totalTarefaPendente = tarefas?.filter(p => p.status === 'PENDENTE').length || 0;
+
+
+ 
+  const totalUsuarioAtivo = usuarios?.filter(p => p.status === 'ATIVO').length || 0;
+  const totalUsuarioInativo = usuarios?.filter(p => p.status === 'INATIVO').length || 0;
+
+  
 
   return (
     <Container className="mt-4">
@@ -107,7 +121,7 @@ export default function Dashboard() {
                     <Card className="shadow-sm border-0 border-start border-4 border-primary h-100">
                       <Card.Header className="text-muted text-uppercase fw-bold mb-2">ATIVO</Card.Header>
                       <Card.Body className="mb-3">
-                        <h3 className="mb-0 fw-bold text-dark">5</h3>
+                        <h3 className="mb-0 fw-bold text-dark">{totalTarefaAtivo}</h3>
                       </Card.Body>
                     </Card>
                   </Col>
@@ -116,16 +130,16 @@ export default function Dashboard() {
                     <Card className="shadow-sm border-0 border-start border-4 border-primary h-100">
                       <Card.Header className="text-muted text-uppercase fw-bold mb-2">SUPENSO</Card.Header>
                       <Card.Body className="mb-3">
-                        <h3 className="mb-0 fw-bold text-dark"></h3>
+                        <h3 className="mb-0 fw-bold text-dark">{totalTarefaSuspenso}</h3>
                       </Card.Body>
                     </Card>
                   </Col>
 
                   <Col xs={12} md={6} lg={3}>
                     <Card className="shadow-sm border-0 border-start border-4 border-primary h-100">
-                      <Card.Header className="text-muted text-uppercase fw-bold mb-2">INATIVO</Card.Header>
+                      <Card.Header className="text-muted text-uppercase fw-bold mb-2">PENDENTE</Card.Header>
                       <Card.Body className="mb-3">
-                        <h3 className="mb-0 fw-bold text-dark">0</h3>
+                        <h3 className="mb-0 fw-bold text-dark">{totalTarefaPendente}</h3>
                       </Card.Body>
                     </Card>
                   </Col>
@@ -134,7 +148,7 @@ export default function Dashboard() {
                     <Card className="shadow-sm border-0 border-start border-4 border-primary h-100">
                       <Card.Header className="text-muted text-uppercase fw-bold mb-2">CANCELADOS</Card.Header>
                       <Card.Body className="mb-3">
-                        <h3 className="mb-0 fw-bold text-dark">0</h3>
+                        <h3 className="mb-0 fw-bold text-dark">{totalTarefaCancelado}</h3>
                       </Card.Body>
                     </Card>
                   </Col>
@@ -153,17 +167,9 @@ export default function Dashboard() {
                 <Row className="g-3">
                   <Col xs={12} md={6} lg={3}>
                     <Card className="shadow-sm border-0 border-start border-4 border-dark h-100">
-                      <Card.Header className="text-muted text-uppercase fw-bold mb-2">TOTAL</Card.Header>
-                      <Card.Body className="mb-3">
-                        <h3 className="mb-0 fw-bold text-dark">5</h3>
-                      </Card.Body>
-                    </Card>
-                  </Col>
-                  <Col xs={12} md={6} lg={3}>
-                    <Card className="shadow-sm border-0 border-start border-4 border-dark h-100">
                       <Card.Header className="text-muted text-uppercase fw-bold mb-2">ATIVO</Card.Header>
                       <Card.Body className="mb-3">
-                        <h3 className="mb-0 fw-bold text-dark">5</h3>
+                        <h3 className="mb-0 fw-bold text-dark">{totalUsuarioAtivo}</h3>
                       </Card.Body>
                     </Card>
                   </Col>
@@ -172,7 +178,7 @@ export default function Dashboard() {
                     <Card className="shadow-sm border-0 border-start border-4 border-dark h-100">
                       <Card.Header className="text-muted text-uppercase fw-bold mb-2">INATIVO</Card.Header>
                       <Card.Body className="mb-3">
-                        <h3 className="mb-0 fw-bold text-dark">12</h3>
+                        <h3 className="mb-0 fw-bold text-dark">{totalUsuarioInativo}</h3>
                       </Card.Body>
                     </Card>
                   </Col>
